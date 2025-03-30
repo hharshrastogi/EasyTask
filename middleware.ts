5e4d80a5d440344f234aacb3cf158425438c8102
Harsh <hharshrastogi@gmail.com>
2025-03-30 17:55:35 +0530

    Added Organizations

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,30 @@
+import { RedirectToSignIn } from '@clerk/nextjs'
 import { clerkMiddleware } from '@clerk/nextjs/server'
-
+import { NextResponse } from 'next/server'
 export default clerkMiddleware({
-  publicRoutes: ["/p"]
+  publicRoutes: ["/p"],
+  afterAuth(auth, req){
+    if (auth.userId && auth.isPublicRoute(req)) {
+      let path = "/select-org";
+
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
+
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+
+    if (!auth.userId && !auth.isPublicRoute(req)) {
+      return RedirectToSignIn({ redirectUrl: req.url });
+    }
+
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+      const orgSelection = new URL("/select-org", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+    return NextResponse.next();
+  }
 })
 
 export const config = {
